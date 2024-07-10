2f144ff643f610e784bac7a1ace285604b09516a
Lucas Castaño Jaramillo <38567515+lcastanoj@users.noreply.github.com>
2024-07-10 15:24:35 -0500

    Update WaitUtils.java

--- src/main/java/com/haceb/utils/WaitUtils.java
+++ src/main/java/com/haceb/utils/WaitUtils.java
@@ -1,12 +1,28 @@
 package com.haceb.utils;
 
-public class WaitUtils {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-    public static void waitForSeconds(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+public class WaitUtils {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    public void waitForElementPresent(By locator) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public void waitForElementClickable(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void waitForTextInElement(By locator, String text) {
+        wait.until(ExpectedConditions.textToBe(locator, text));
     }
 }
